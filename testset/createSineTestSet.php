bafee74b05ab8f985288cb6e6284179af63048cd
scjurgen <scjurgen@yahoo.com>
2013-03-15 17:12:47 +0100

    view deck added

--- testset/createSineTestSet.php
+++ testset/createSineTestSet.php
@@ -5,15 +5,17 @@
     $facx=3.1415926*3.0;
     $facy=3.1415926*5.0;
     $scale=0.9;
-    for ($a=-1.0; $a <= 1.0; $a+=0.025)
+    $yscale=0.0;
+    $step=0.1;
+    for ($a=-1.0; $a <= 1.0; $a+=$step)
     {
-        $x=(sin($a * $facx)+$scale)/($scale*2.0);
-        $y=(cos($a * $facy)+$scale)/($scale*2.0);
+        $x=(sin($a * $facx)+$yscale)/($scale*2.0);
+        $y=(cos($a * $facy)+$yscale)/($scale*2.0);
         fprintf($fp,"%f,%f,%f\\n",$a,$x,$y);
     }
     $a=1.0;
-    $x=(sin($a * $facx)+$scale)/($scale*2.0);
-    $y=(cos($a * $facy)+$scale)/($scale*2.0);
+    $x=(sin($a * $facx)+$yscale)/($scale*2.0);
+    $y=(cos($a * $facy)+$yscale)/($scale*2.0);
     fprintf($fp,"%f,%f,%f\\n",$a,$x,$y);
-    fprintf($fp,"#\"");
+    fprintf($fp,"\"");
 ?>
